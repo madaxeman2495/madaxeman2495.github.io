60645cab2daeed812774919859e16e8dafb1ff66
madaxeman2495 <madaxeman2495@users.noreply.github.com>
2023-03-08 13:32:53 +0000

    Add files via upload

---
+++ index_nof.js
@@ -0,0 +1,14 @@
+// Begin Actions
+function F_doLoaded() {
+	document.main = new F_cMain();
+	document.objectModel = new Object();
+	F_OM('Layout','LayoutLYR', 'doc', '', new Array(
+	'Page Loaded','Layout','Go To URL',F_Parm('URL','http://madaxeman2495.github.io/avh_current.pdf','Target Frame','This','Other Target',''),0));
+	F_pageLoaded('Layout');
+}
+ 
+$(document).ready( function() {
+F_onLoaded();
+});
+// End Actions
+
